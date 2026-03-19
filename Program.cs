628f7b3dcd33e6891ddc452ac2113e9acb959f87
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-03-18 19:03:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KubeCRDStoreProxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
